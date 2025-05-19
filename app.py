3cc7efd32b41d6d179e8eefdb8835f7cc9549b8d
AdrianEsgate-301 <aesgate22@mail.wou.edu>
2025-05-18 17:32:40 -0700

    submission

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+@app.route("/api/upper/<string:string>", methods=["GET"])
+def method_name(string):
+    try:
+        upper_string = string.upper()
+        return {"result": upper_string}, 200
+    except:
+        return {"result": ""}, 404
+
+@app.route("/api/lower/<string:string>", methods=["GET"])
+def method_names(string):
+    try:
+        lower_string = string.lower()
+        return {"result": lower_string}, 200
+    except:
+        return {"result": ""}, 400
